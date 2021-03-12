84e4a37209fd25b8faf72e6dfb921cb55ef1ce87
tfernandez2017 <44676635+tfernandez2017@users.noreply.github.com>
2021-03-12 11:35:50 -0500

    JS - Playback Speed

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
+const speed = document.querySelector('.player-speed');
 const fullscreenBtn = document.querySelector('.fullscreen');
+
 
 // Play & Pause ----------------------------------- //
 
@@ -102,7 +104,9 @@ function toggleMute() {
 
 // Change Playback Speed -------------------- //
 
-
+function changeSpeed() {
+    video.playbackRate = speed.value;
+}
 
 // Fullscreen ------------------------------- //
 
@@ -114,5 +118,6 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
 
 
